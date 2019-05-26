9c8ffc8b597bb6dcadf87ac9577735b3fffb6b15
Eliot Howes <ejhowes@gmail.com>
2019-05-26 16:56:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect(array)
   counter = 0
-  while counter < array.length
-
-  counter += 1
-  end
+    while counter < array.length
+      yield(array[counter].split(" "))
+     counter += 1
+    end
 end

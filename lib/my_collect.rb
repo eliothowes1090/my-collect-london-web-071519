8593a3a7c503890fae5079d9a6223e05efc1b469
Eliot Howes <ejhowes@gmail.com>
2019-05-26 17:23:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,7 @@
 def my_collect(collection)
   counter = 0
-  new_collection = []
     while counter < collection.length
-      yield(new_collection.push(collection[counter]))
+      yield(collection[counter])
      counter += 1
     end
-  return new_collection
 end
